41d60217c903794e3b9cfa114a65bd70e228a503
BlackHe <1124681011@qq.com>
2018-09-17 23:17:15 +0800

    完善微服务集群

--- microcloud-mall-80/src/main/java/com/peony/microcloudmall/controller/JdMallController.java
+++ microcloud-mall-80/src/main/java/com/peony/microcloudmall/controller/JdMallController.java
@@ -4,6 +4,7 @@ import com.peony.entity.Product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +34,7 @@ public class JdMallController {
     @RequestMapping("/page/{productId}")
     public String showHomePage(@PathVariable @NotNull String productId, Map<String,Object>  attributeMap){
 //        Product product =  restTemplate.getForObject("http://localhost:8001/product/get/"+productId,Product.class,productId);
+        /** http://MICROCLOUD-STOCK/ 是微服务提供者的服务名，可能是集群环境，但注册在eureka上的服务名是相同的*/
         Product product =  restTemplate.getForObject("http://MICROCLOUD-STOCK/product/get/"+productId,Product.class,productId);
         log.info("从库存微服务 http://MICROCLOUD-STOCK/ 获取到的商品信息 = {}",product.toString());
         attributeMap.put("product",product);
--- microcloud-stock-8002/src/test/java/com/peony/microcloudstock/MicrocloudStock8002ApplicationTests.java
+++ microcloud-stock-8002/src/test/java/com/peony/microcloudstock/MicrocloudStock8002ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.peony.microcloudorder;
+package com.peony.microcloudstock;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MicrocloudOrder8002ApplicationTests {
+public class MicrocloudStock8002ApplicationTests {
 
     @Test
     public void contextLoads() {
