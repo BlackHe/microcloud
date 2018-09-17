482f76c20790310bca12eb7df7a433f9c5363e95
BlackHe <1124681011@qq.com>
2018-09-17 21:05:42 +0800

    增加eureka集群服务，本次配置3个eureka服务组成的小集群

---
+++ microcloud-eureka-7002/src/main/java/com/peony/microcloudeureka/MicrocloudEureka7002Application.java
@@ -0,0 +1,12 @@
+package com.peony.microcloudeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrocloudEureka7002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrocloudEureka7002Application.class, args);
+    }
+}
---
+++ microcloud-eureka-7003/src/main/java/com/peony/microcloudeureka/MicrocloudEureka7003Application.java
@@ -0,0 +1,12 @@
+package com.peony.microcloudeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrocloudEureka7003Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrocloudEureka7003Application.class, args);
+    }
+}
