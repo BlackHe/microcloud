d49844ff7bf3432591c24f14a718a79aeaedc399
BlackHe <11124681011@qq.com>
2020-12-20 18:09:31 +0800

    一、spring程序中多种注入方式 1.@Configuration+@Bean  方法级别 2.@Import + 自定义的ImportSelector 类级别，适合注入三方库的类 3.@Import + 自定义的ImportBeanDefinitionRegistry 类级别，适合注入三方库的类 二、注入条件：@Condition 1.可以根据不同的系统环境，不同的第三方依赖等，有条件的进行注入 2.很适合在spring.factories场景下使用 三、springboot 扩展机制 1.很灵活的spring.factories机制，是实现springboot自动装配的核心 2.可以用这个机制，实现自己的starter,也可以自己写底层类库，提供给也业务方使用

--- microcloud-stock-8001/src/main/java/com/peony/microcloudstock/MicrocloudStock8001Application.java
+++ microcloud-stock-8001/src/main/java/com/peony/microcloudstock/MicrocloudStock8001Application.java
@@ -1,14 +1,27 @@
 package com.peony.microcloudstock;
 
+import com.peonys.peonystarter.configure.MetricService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class MicrocloudStock8001Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicrocloudStock8001Application.class, args);
+        ConfigurableApplicationContext ac = SpringApplication.run(MicrocloudStock8001Application.class, args);
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println(beanDefinitionName);
+        }
+        MetricService bean = ac.getBean(MetricService.class);
+        System.out.println(bean);
     }
 }
